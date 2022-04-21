644007978a9904db1b28ff205b8a644f4673c5d8
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-04-22 00:42:51 +0300

    add swagger2, Db connection

--- src/main/java/com/turnuvamvar/thesis/ThesisApplication.java
+++ src/main/java/com/turnuvamvar/thesis/ThesisApplication.java
@@ -1,13 +1,28 @@
 package com.turnuvamvar.thesis;
 
+import com.sun.xml.bind.v2.runtime.output.SAXOutput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+
 public class ThesisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThesisApplication.class, args);
+	}
+	@Bean
+	public Docket api() {		// Bunu config paketinde tut.
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.turnuvamvar.thesis"))
+				.build();
 	}
 
 }
