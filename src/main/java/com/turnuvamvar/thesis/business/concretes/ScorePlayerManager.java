f189b48a17567566d96db4ae5045fa2dc91c0a8f
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-06-02 02:24:44 +0300

    edit update and delete methods of ScorePlayer

--- src/main/java/com/turnuvamvar/thesis/business/concretes/ScorePlayerManager.java
+++ src/main/java/com/turnuvamvar/thesis/business/concretes/ScorePlayerManager.java
@@ -2,25 +2,31 @@ package com.turnuvamvar.thesis.business.concretes;
 
 import com.turnuvamvar.thesis.business.abstracts.ScorePlayerService;
 import com.turnuvamvar.thesis.core.utilities.results.*;
+import com.turnuvamvar.thesis.dataAccess.abstracts.PlayerDao;
+import com.turnuvamvar.thesis.dataAccess.abstracts.ScoreDao;
 import com.turnuvamvar.thesis.dataAccess.abstracts.ScorePlayerDao;
 import com.turnuvamvar.thesis.dto.Request.ScorePlayerRequestDto;
 import com.turnuvamvar.thesis.dto.Response.ScorePlayerResponseDto;
+import com.turnuvamvar.thesis.entities.concretes.Player;
+import com.turnuvamvar.thesis.entities.concretes.Score;
 import com.turnuvamvar.thesis.entities.concretes.ScorePlayer;
 import com.turnuvamvar.thesis.mapper.Request.ScorePlayerRequestMapper;
 import com.turnuvamvar.thesis.mapper.Response.ScorePlayerResponseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 @Service
 public class ScorePlayerManager implements ScorePlayerService {
     @Autowired
     private ScorePlayerDao scorePlayerDao;
     private ScorePlayerResponseMapper scorePlayerResponseMapper;
     private ScorePlayerRequestMapper scorePlayerRequestMapper;
+    private PlayerDao playerDao;
+    private ScoreDao scoreDao;
     @Autowired
     public ScorePlayerManager(ScorePlayerDao scorePlayerDao) {
         this.scorePlayerDao = scorePlayerDao;
@@ -33,16 +39,32 @@ public class ScorePlayerManager implements ScorePlayerService {
     public void setScorePlayerRequestMapper(ScorePlayerRequestMapper scorePlayerRequestMapper) {
         this.scorePlayerRequestMapper = scorePlayerRequestMapper;
     }
+    @Autowired
+    public void setPlayerDao(PlayerDao playerDao) {
+        this.playerDao = playerDao;
+    }
+    @Autowired
+    public void setScoreDao(ScoreDao scoreDao) {
+        this.scoreDao = scoreDao;
+    }
 
     @Override
     public DataResult<ScorePlayerResponseDto> createOneScorePlayer(ScorePlayerRequestDto newScorePlayerRequestDto) {
         // kontroller! öyle bir oyuncu var mı vs.
-        if(newScorePlayerRequestDto == null){
-            ScorePlayer scorePlayer = this.scorePlayerRequestMapper.mapScorePlayerRequestDtoToScorePlayer(newScorePlayerRequestDto);
-            // setlemek gereken bir şey var mı!
-            scorePlayer = this.scorePlayerDao.save(scorePlayer);
-            ScorePlayerResponseDto scorePlayerResponseDto = this.scorePlayerResponseMapper.mapScorePlayerToScorePlayerResponseDto(scorePlayer);
-            return new SuccessDataResult<ScorePlayerResponseDto>(scorePlayerResponseDto);
+        Optional<Player> player = this.playerDao.findById(newScorePlayerRequestDto.getPlayerId());
+        Optional<Score> score = this.scoreDao.findById(newScorePlayerRequestDto.getScoreId());
+        if(newScorePlayerRequestDto != null){
+            if(player.isPresent() && score.isPresent()){
+                ScorePlayer scorePlayer = this.scorePlayerRequestMapper.mapScorePlayerRequestDtoToScorePlayer(newScorePlayerRequestDto);
+                scorePlayer.setPlayer(player.get());
+                scorePlayer.setScore(score.get());
+                scorePlayer = this.scorePlayerDao.save(scorePlayer);
+                ScorePlayerResponseDto scorePlayerResponseDto = this.scorePlayerResponseMapper.mapScorePlayerToScorePlayerResponseDto(scorePlayer);
+                return new SuccessDataResult<ScorePlayerResponseDto>(scorePlayerResponseDto);
+            }else{
+                return new ErrorDataResult<ScorePlayerResponseDto>("oyuncu ya da score bulunamadı..");
+            }
+
         }else{
             return new ErrorDataResult<ScorePlayerResponseDto>("istek null");
         }
@@ -75,8 +97,27 @@ public class ScorePlayerManager implements ScorePlayerService {
     }
 
     @Override
-    public DataResult<ScorePlayerResponseDto> updateOneScorePlayer(Long scorePlayerId, ScorePlayerRequestDto scorePlayerResponseDto) {
-        return null;
+    public DataResult<ScorePlayerResponseDto> updateOneScorePlayer(Long scorePlayerId, ScorePlayerRequestDto scorePlayerRequestDto) {
+        Optional<ScorePlayer> scorePlayer = this.scorePlayerDao.findById(scorePlayerId);
+        Optional<Player> player = this.playerDao.findById(scorePlayerRequestDto.getPlayerId());
+        Optional<Score> score = this.scoreDao.findById(scorePlayerRequestDto.getScoreId());
+        if(scorePlayer.isPresent()){
+            if(player.isPresent() && score.isPresent()){
+                ScorePlayer toSave = scorePlayer.get();
+                toSave.setPlayer(player.get());
+                toSave.setScore(score.get());
+                toSave = this.scorePlayerDao.save(toSave);
+                ScorePlayerResponseDto scorePlayerResponseDto = this.scorePlayerResponseMapper.mapScorePlayerToScorePlayerResponseDto(toSave);
+                return new SuccessDataResult<ScorePlayerResponseDto>(scorePlayerResponseDto);
+            }
+            else{
+                return new ErrorDataResult<ScorePlayerResponseDto>("oyuncu ya da score bulunamadı..");
+            }
+
+        }
+        else{
+            return new ErrorDataResult<ScorePlayerResponseDto>("ScorePlayer bulunamadı..");
+        }
     }
 
     @Override
@@ -90,4 +131,5 @@ public class ScorePlayerManager implements ScorePlayerService {
             return new ErrorResult("score_player bulunamadığı için silinemedi!");
         }
     }
+
 }
---
+++ src/main/java/com/turnuvamvar/thesis/dataAccess/abstracts/ScoreDao.java
@@ -0,0 +1,7 @@
+package com.turnuvamvar.thesis.dataAccess.abstracts;
+
+import com.turnuvamvar.thesis.entities.concretes.Score;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScoreDao extends JpaRepository<Score,Long> {
+}
